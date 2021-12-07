f5bd6443971f487f3f8ca6fdc483029c0f945b3a
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-12-07 22:24:14 +0800

    使用create方法

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,15 @@ use App\Models\Post;
 */
 
 Route::get('/', function (){
-    $post=new Post();
-    $post->title ='test title';
-    $post->content='test content';
-    $post->save();
+//    $post=new Post();
+//    $post->title ='test title';
+//    $post->content='test content';
+//    $post->save();
+
+    Post::create([
+        'title'=>'test title2',
+        'content'=>'test content2'
+    ]);
 });
 
 Route::get('posts', [Controllers\IndexController::class, 'index'])->name('posts.index');
