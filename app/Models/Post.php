82ff4d7213382259311a123c0346caf8bad09299
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-12-08 01:46:51 +0800

    透過comments關係，擷取comments資料

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,11 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $table='posts';
+    protected $table='posts'; //多餘，可略
+//    protected $fillable=[
+//        'title',
+//        'content',
+//        'is_feature'
+//    ];
 
-    protected $fillable=[
-        'title',
-        'content',
-        'is_feature'
-    ];
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+
 }
